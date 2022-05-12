639bacdae89347f2530965cb41a83e101b3126d6
sunilr80 <cyhurg@gmail.com>
2022-05-12 08:08:31 +0530

    Database creation

---
+++ NZWalks.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,20 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksDbContext: DbContext
+    {
+        public NZWalksDbContext(DbContextOptions<NZWalksDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Walk>  Walks { get; set; }
+
+        public DbSet<Region> Regions { get; set; }
+
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
